cb4a8cda0298133f14dc847fd1ad80999cd87b46
eduxcellence <33365906+eduxcellence@users.noreply.github.com>
2020-05-02 20:17:05 +0530

    Updated  Implicit Intent Module

--- app/src/main/java/source/open/akash/kotlinexample/Intent/ImlicitIntentActivity/ImplicitActivity.kt
+++ app/src/main/java/source/open/akash/kotlinexample/Intent/ImlicitIntentActivity/ImplicitActivity.kt
@@ -1,13 +1,175 @@
 package source.open.akash.kotlinexample.Intent.ImlicitIntentActivity
 
+import android.Manifest
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import kotlinx.android.synthetic.main.activity_implicit.*
+import source.open.akash.kotlinexample.ManagePermissions
 import source.open.akash.kotlinexample.R
+import java.io.IOException
 
 class ImplicitActivity : AppCompatActivity() {
+
+    val CAMERAREQUEST = 225
+    val REQUEST_GALLERY_CODE = 335
+
+    companion object {
+        var permissionGrant: Boolean = false
+    }
+
+    private val PermissionsRequestCode = 123
+    private lateinit var managePermissions: ManagePermissions
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_implicit)
+        //for using camera ,we have to permissions from user as per policy of mobile devices
+        // from Marshmallow permissions are
+        // Manifest.permission.CAMERA,
+        // Manifest.permission.WRITE_EXTERNAL_STORAGE
+        setupPermissions()
+        implicitThree.setOnClickListener {
+            if (!permissionGrant) {
+                setupPermissions()
+            } else {
+                val intentCamera = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                startActivityForResult(intentCamera, CAMERAREQUEST)
+            }
+
+        }
+
+        implicitOne.setOnClickListener {
+            val mailIntent = Intent(Intent.ACTION_SEND)
+            mailIntent.data = Uri.parse("Email")
+
+            //for multiple recipients
+            val contactsArray = arrayOf("abc@gmail.com", "def@gmail.com", "ghi@gmail.com")
+            mailIntent.putExtra(Intent.EXTRA_EMAIL, contactsArray)
+            mailIntent.putExtra(Intent.EXTRA_SUBJECT, "Put Your Subject Here")
+            mailIntent.putExtra(Intent.EXTRA_TEXT, "Put Your Email Content")
+            mailIntent.type = "message/rfc822"
+            val sendMail = Intent.createChooser(mailIntent, "Launch your Mail using Below APP's")
+            startActivity(sendMail)
+        }
+
+        implicitTwo.setOnClickListener {
+            val mapIntent = Intent(Intent.ACTION_VIEW)
+            mapIntent.data = Uri.parse("geo:26.70,80.19")
+            val startMap = Intent.createChooser(mapIntent, "Launch Maps")
+            startActivity(startMap)
+        }
+        implicitFour.text = "PHONE CALL : 8888888888"
+        implicitFour.setOnClickListener {
+            val callIntent = Intent(Intent.ACTION_CALL)
+            callIntent.data = Uri.parse("tel:" + 8888888888)
+            if (ActivityCompat.checkSelfPermission(
+                    this@ImplicitActivity,
+                    android.Manifest.permission.CALL_PHONE
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                toast("Grant Permission to Call")
+                requestCallPermission()
+            } else {
+                startActivity(callIntent)
+            }
+        }
+        implicitFive.setOnClickListener {
+            val openGalleryIntent = Intent()
+            openGalleryIntent.type = "image/*"
+            openGalleryIntent.action = Intent.ACTION_GET_CONTENT
+            startActivityForResult(
+                Intent.createChooser(openGalleryIntent, "Select Image "),
+                REQUEST_GALLERY_CODE
+            )
+
+        }
+    }
+
+    fun requestCallPermission() {
+        ActivityCompat.requestPermissions(
+            this@ImplicitActivity,
+            arrayOf(android.Manifest.permission.CALL_PHONE), 1
+        )
+    }
+
+    private fun setupPermissions() {
+        // Initialize a list of required permissions to request runtime
+        val list = listOf<String>(
+            Manifest.permission.CAMERA,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+        )
+        // Initialize a new instance of ManagePermissions class
+        managePermissions = ManagePermissions(this, list, PermissionsRequestCode)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+            managePermissions.checkPermissions()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == CAMERAREQUEST && resultCode == Activity.RESULT_OK) {
+            val imageBitmap = data!!.extras!!.get("data") as Bitmap
+            imageDemo.setImageBitmap(imageBitmap)
+        } else if (requestCode == REQUEST_GALLERY_CODE && resultCode == Activity.RESULT_OK) {
+            if (data != null) {
+                try {
+                    val bitmapImage: Bitmap =
+                        MediaStore.Images.Media.getBitmap(application.contentResolver, data.data)
+                    imageDemo.setImageBitmap(bitmapImage)
+
+                } catch (exc: IOException) {
+                    exc.printStackTrace()
+                }
+            }
+        } else if (resultCode == Activity.RESULT_CANCELED) {
+            toast("Cancelled")
+        }
+    }
+
+    // Receive the permissions request result
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            PermissionsRequestCode -> {
+                val isPermissionsGranted = managePermissions
+                    .processPermissionsResult(requestCode, permissions, grantResults)
+
+                if (isPermissionsGranted) {
+                    // Do the task now
+                    permissionGrant = true
+                    toast("Permissions granted.")
+                } else {
+                    permissionGrant = false
+                    toast("Permissions denied.")
+                }
+                return
+            }
+        }
     }
 }
+
+
+// Extension function to show toast message
+fun Context.toast(message: String) {
+    if (message.equals("Permissions already granted.")) {
+        ImplicitActivity.permissionGrant = true
+    } else {
+        ImplicitActivity.permissionGrant = false
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+
+}
+
---
+++ app/src/main/java/source/open/akash/kotlinexample/ManagePermissions.kt
@@ -0,0 +1,87 @@
+package source.open.akash.kotlinexample
+
+import android.app.Activity
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import source.open.akash.kotlinexample.Intent.ImlicitIntentActivity.toast
+
+/**
+ * Created by Akash Kumar  of Eduxcellence on 5/2/2020.
+ */
+
+class ManagePermissions(val activity: Activity, val list: List<String>, val code: Int) {
+
+    // Check permissions at runtime
+    fun checkPermissions() {
+        if (isPermissionsGranted() != PackageManager.PERMISSION_GRANTED) {
+            showAlert()
+        } else {
+            activity.toast("Permissions already granted.")
+        }
+    }
+
+
+    // Check permissions status
+    private fun isPermissionsGranted(): Int {
+        // PERMISSION_GRANTED : Constant Value: 0
+        // PERMISSION_DENIED : Constant Value: -1
+        var counter = 0
+        for (permission in list) {
+            counter += ContextCompat.checkSelfPermission(activity, permission)
+        }
+        return counter
+    }
+
+
+    // Find the first denied permission
+    private fun deniedPermission(): String {
+        for (permission in list) {
+            if (ContextCompat.checkSelfPermission(activity, permission)
+                == PackageManager.PERMISSION_DENIED
+            ) return permission
+        }
+        return ""
+    }
+
+
+    // Show alert dialog to request permissions
+    private fun showAlert() {
+        val builder = AlertDialog.Builder(activity)
+        builder.setTitle("Need permission(s)")
+        builder.setMessage("Some permissions are required to do the task.")
+        builder.setPositiveButton("OK", { dialog, which -> requestPermissions() })
+        builder.setNeutralButton("Cancel", null)
+        val dialog = builder.create()
+        dialog.show()
+    }
+
+
+    // Request the permissions at run time
+    private fun requestPermissions() {
+        val permission = deniedPermission()
+        if (ActivityCompat.shouldShowRequestPermissionRationale(activity, permission)) {
+            // Show an explanation asynchronously
+            activity.toast("Should show an explanation.")
+        } else {
+            ActivityCompat.requestPermissions(activity, list.toTypedArray(), code)
+        }
+    }
+
+
+    // Process permissions result
+    fun processPermissionsResult(
+        requestCode: Int, permissions: Array<String>,
+        grantResults: IntArray
+    ): Boolean {
+        var result = 0
+        if (grantResults.isNotEmpty()) {
+            for (item in grantResults) {
+                result += item
+            }
+        }
+        if (result == PackageManager.PERMISSION_GRANTED) return true
+        return false
+    }
+}
